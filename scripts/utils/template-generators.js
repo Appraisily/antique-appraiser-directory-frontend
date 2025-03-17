ef5bf08982b284c3c3bd39aec791414ebe5e2cd6
Appraisily <134934621+Appraisily@users.noreply.github.com>
2025-03-17 20:22:24 +0100

    aa

--- scripts/utils/template-generators.js
+++ scripts/utils/template-generators.js
@@ -239,6 +239,13 @@ export async function generateLocationPageHTML({
     <!DOCTYPE html>
     <html lang="en">
     <head>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PSLHDGM');</script>
+      <!-- End Google Tag Manager -->
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       ${resourceHints}
@@ -247,6 +254,10 @@ export async function generateLocationPageHTML({
       ${schemaMarkup}
     </head>
     <body>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSLHDGM"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
       <header>
         <div class="container">
           <a href="/" class="logo">Art Appraiser Directory</a>
@@ -488,6 +499,13 @@ export async function generateAppraiserPageHTML(appraiser) {
     <!DOCTYPE html>
     <html lang="en">
     <head>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PSLHDGM');</script>
+      <!-- End Google Tag Manager -->
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       ${generateMetaTags(seoData)}
@@ -495,6 +513,10 @@ export async function generateAppraiserPageHTML(appraiser) {
       ${schemaMarkup}
     </head>
     <body>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSLHDGM"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
       <header>
         <div class="container">
           <a href="/" class="logo">Art Appraiser Directory</a>
